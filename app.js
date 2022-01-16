b8fcefcca7c22f1e05c0c94b9bfc8b61e253956c
Chris Morris <chrismorris08@gmail.com>
2022-01-16 14:40:02 -0600

    Added static react content to single app.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const cors = require('cors')
 const path = require('path');
 const logger = require('morgan');
+const publicPath = __dirname + '/app/views/';
 const app = express();
 const apiRouter = require('./api/routes');
 
@@ -29,8 +30,11 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(cors())
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'web', 'public')));
+app.use(express.static(publicPath));
 app.use('/api', apiRouter);
+app.get('/', function (req,res) {
+    res.sendFile(path + "index.html");
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
